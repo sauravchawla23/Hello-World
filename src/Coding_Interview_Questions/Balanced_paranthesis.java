919670601d8942be743e5a1b2f8ff60e1df6823e
Saurav Chawla <sauravchawla23@gmail.com>
2025-06-30 22:30:24 +0530

    new questions added

---
+++ src/Coding_Interview_Questions/Balanced_paranthesis.java
@@ -0,0 +1,41 @@
+package Coding_Interview_Questions;
+
+import java.util.Stack;
+
+public class Balanced_paranthesis {
+    public static void main(String[] args) {
+        String input = "{[()]}()";
+        if (isBalanced(input)) {
+            System.out.println("Balanced");
+        } else {
+            System.out.println("Not Balanced");
+        }
+    }
+
+    public static boolean isBalanced(String str) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char ch : str.toCharArray()) {
+            // If opening bracket, push to stack
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            }
+            // If closing bracket, check stack
+            else if (ch == ')' || ch == '}' || ch == ']') {
+                if (stack.isEmpty()) return false;
+
+                char top = stack.pop();
+                if ((ch == ')' && top == '(') ||
+                        (ch == '}' && top == '{') ||
+                        (ch == ']' && top == '[')) {
+                    continue;
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+
+        return stack.isEmpty(); // true if all matched
+    }
+}
