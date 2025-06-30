ba0df8ac8072993e7a27639098f8fd6bd6cd6e29
Saurav Chawla <sauravchawla23@gmail.com>
2025-06-30 13:08:40 +0530

    some editing

--- src/Coding_Interview_Questions/Sum_of_Two_digit_target.java
+++ src/Coding_Interview_Questions/Sum_of_Two_digit_target.java
@@ -1,10 +1,19 @@
 package Coding_Interview_Questions;
 
+import java.util.Scanner;
+
 public class Sum_of_Two_digit_target {
     // 1,2,3,4 ,5 target 7 - pairs are 2,5 and 3,4 solve this by two pointer approach
     public static void main(String[] args) {
-        int arr[] = {1,2,3,4,5};
-        int target = 7;
+
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        int target = sc.nextInt();
         int s = 0 ;
         int e=arr.length-1;
 
